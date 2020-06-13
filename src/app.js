c321f637e9a58755e996d9840f8e5a0b6f3806ee
Nick <nickmarron@hotmail.co.uk>
2020-06-13 11:23:32 +0100

    strings completed

--- src/app.js
+++ src/app.js
@@ -1,35 +1,30 @@
 const express = require('express');
-const { sayHello } = require('./lib/strings.js');
-const { upperCase } = require('./lib/strings.js');
-const { lowercase } = require('./lib/strings.js');
-const { firstCharacter } = require('./lib/strings.js');
-const { firstCharacters } = require('./lib/strings.js');
+const argsString = require('./lib/strings');
 
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
-  res.json({ result: sayHello(req.params.string) });
+  res.json({ result: argsString.sayHello(req.params.string) });
   // res.status(200).json({ result: 'Hello world!' });
 });
 
 app.get('/strings/upper/:string', (req, res) => {
-  res.json({ result: upperCase(req.params.string) });
+  res.json({ result: argsString.upperCase(req.params.string) });
   // res.status(200).json({ result: 'HELLO' });
 });
 
 app.get('/strings/lower/:string', (req, res) => {
-  res.json({ result: lowercase(req.params.string) });
+  res.json({ result: argsString.lowercase(req.params.string) });
   // res.status(200).json({ result: 'hello' });
 });
 
-app.get('/strings/firstcharacter/:string', (req, res) => {
-  res.json({ result: firstCharacter(req.params.string) });
-  // res.status(200).json({ result: 'h' });
-});
-
 app.get('/strings/first-characters/:string', (req, res) => {
-  res.json({ result: firstCharacters(req.params.string, req.query.length) });
-  // res.status(200).json({ result: 'sd32' });
+  if (req.query.length === undefined) {
+    res.json({ result: argsString.firstCharacter(req.params.string) });
+  } else {
+    res.json({ result: argsString.firstCharacters(req.params.string, req.query.length) });
+  }
+  // res.status(200).json({ result: 'h' });
 });
 
 module.exports = app;
